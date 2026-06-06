312746894cfa20c33de4e68ff578294d968a4ab2
Sid46a <shaikabubakarsiddiq111@gmail.com>
2026-06-07 01:38:05 +0530

    Componet Composion

--- App.js
+++ App.js
@@ -1,31 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//Functional Components
+//Functional Component Composition
 
 
+//Component One 
+const Title = ()=> (
+     <h1>Title</h1>
+)
 
-// function Heading(){
-//     return <h1>Hello Functional Components</h1>;
-// }
-
-const Heading1=() =>{ return( 
+// Component Two
+const Heading=() =>{ return( 
 
     <div  id="Container">
-    <h1 id="Heading1">Hello Arrow Functional components</h1>
-    <h2 id="Heading2">hello siddiq</h2>
-    <h3 id="Heading3">Xyang</h3>
+
+     <Title/> { /*Composing the components with one another* */}
+
+    <h1 id="Heading1">Heading</h1>
+    
     </div>
 )
 }
 
 
-// const Heading2=()=> <h1>Hello arrow shortcut functional component</h1>;
-
-
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(<Heading2/>);
+root.render(<Heading/>);
 
 
 console.log("Hello siddiq");
